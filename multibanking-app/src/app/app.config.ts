067f08b96bf1070f7c18473392f92a5563e2f4cf
Alexander Geist <age@adorsys.de>
2017-04-28 13:57:45 +0200

    browser build

--- multibanking-app/src/app/app.config.ts
+++ multibanking-app/src/app/app.config.ts
@@ -2,5 +2,5 @@ import { Injectable } from '@angular/core';
 
 Injectable()
 export class AppConfig{
-  API_URL = 'http://localhost:10021/api/v1';
+  API_URL = 'https://multibanking-service.dev.adorsys.de/api/v1';
 }
