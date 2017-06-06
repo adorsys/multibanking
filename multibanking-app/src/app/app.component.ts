7c185acd0f25949691a7dc23ff849939053c4cfe
Alexander Geist <age@adorsys.de>
2017-06-06 14:27:22 +0200

    ionic/angular upgrade

--- multibanking-app/src/app/app.component.ts
+++ multibanking-app/src/app/app.component.ts
@@ -1,20 +1,23 @@
-import {Component} from '@angular/core';
-import {Platform} from 'ionic-angular';
-import {StatusBar, Splashscreen} from 'ionic-native';
-import {BankAccessListPage} from "../pages/bankaccess/bankAccessList";
+import { Component } from '@angular/core';
+import { Platform } from 'ionic-angular';
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
 
+import {BankAccessListPage} from "../pages/bankaccess/bankAccessList";
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage = BankAccessListPage;
+  rootPage:any = BankAccessListPage;
 
-  constructor(platform: Platform) {
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
-      StatusBar.styleDefault();
-      Splashscreen.hide();
+      statusBar.styleDefault();
+      splashScreen.hide();
     });
   }
 }
+
+
--- multibanking-app/src/app/app.config.ts
+++ multibanking-app/src/app/app.config.ts
@@ -1,5 +1,5 @@
 export const AppConfig = {
   production: false,
-  auth_url: 'https://multibanking-service.dev.adorsys.de/auth',
+  auth_url: 'https://multibanking-keycloak.dev.adorsys.de/auth',
   api_url: 'https://multibanking-service.dev.adorsys.de/api/v1'
 };
