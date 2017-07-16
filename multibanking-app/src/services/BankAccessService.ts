0ce85560e03bbbde4e64194187cf41bbedfe8982
Alexander Geist <age@adorsys.de>
2017-07-16 14:33:31 +0200

    app bankaccess error handling

--- multibanking-app/src/services/BankAccessService.ts
+++ multibanking-app/src/services/BankAccessService.ts
@@ -32,14 +32,10 @@ export class BankAccessService {
   }
 
   handleError(error) {
-    console.error(error.json());
+    console.error(error);
     let errorJson = error.json();
     if (errorJson) {
-      if (errorJson.message == "BANK_ACCESS_ALREADY_EXIST") {
-        return Observable.throw(errorJson.message);
-      } else {
-        return Observable.throw(errorJson || 'Server error');
-      }
+      return Observable.throw(errorJson || 'Server error');
     } else {
       return Observable.throw(error || 'Server error');
     }
