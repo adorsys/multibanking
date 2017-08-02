8811503fe1b7ea21f85e36402b0f5e88a723c59c
Alexander Geist <age@adorsys.de>
2017-08-02 09:28:56 +0200

    multibanking app browser build

--- multibanking-app/src/services/BankAccessService.ts
+++ multibanking-app/src/services/BankAccessService.ts
@@ -3,7 +3,6 @@ import { AppConfig } from '../app/app.config';
 import { Http, Response } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/Rx';
-import { AutoCompleteService } from "ionic2-auto-complete";
 
 @Injectable()
 export class BankAccessService {
