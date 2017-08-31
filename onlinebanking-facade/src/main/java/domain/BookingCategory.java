2f555205316b17dbb2b77cac454e18868b2a66a0
akullpp <akullpp@gmail.com>
2017-08-31 16:25:59 +0200

    Add constructors for deserialization

--- onlinebanking-facade/src/main/java/domain/BookingCategory.java
+++ onlinebanking-facade/src/main/java/domain/BookingCategory.java
@@ -1,13 +1,17 @@
 package domain;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /**
  * Created by alexg on 18.05.17.
  */
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class BookingCategory {
 
     private String mainCategory;
