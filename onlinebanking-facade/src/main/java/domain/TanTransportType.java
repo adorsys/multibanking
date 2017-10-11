9442e2ef9751f5bf422237f6ddbaf2a81bcea04c
Alexander Geist <age@adorsys.de>
2017-10-11 12:10:05 +0200

    fixed missing constructor

--- onlinebanking-facade/src/main/java/domain/TanTransportType.java
+++ onlinebanking-facade/src/main/java/domain/TanTransportType.java
@@ -1,13 +1,17 @@
 package domain;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /**
  * Created by alexg on 11.09.17.
  */
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class TanTransportType {
 
     private String id;
