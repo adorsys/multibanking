0b79d84e899ef70879163d37e93e0dd7d387a606
Alexander Geist <age@adorsys.de>
2017-12-28 14:25:08 +0100

    app upload/download rules fix

--- multibanking-app/src/pages/rules-custom/rulesCustom.ts
+++ multibanking-app/src/pages/rules-custom/rulesCustom.ts
@@ -2,56 +2,38 @@ import { Component, ViewChild } from '@angular/core';
 import { NavController, NavParams, AlertController, Navbar } from 'ionic-angular';
 import { RulesService } from '../../services/RulesService';
 import { Rule } from '../../api/Rule';
-import { RuleEditPage } from '../rule-edit/ruleEdit';
 import { RulesCustomAutoCompleteService } from '../../services/RulesCustomAutoCompleteService';
 import { AutoCompleteComponent } from 'ionic2-auto-complete';
+import { RulesStaticPage } from '../rules-static/rulesStatic';
+import { RulesStaticAutoCompleteService } from '../../services/RulesStaticAutoCompleteService';
 
 @Component({
   selector: 'page-rules-custom',
   templateUrl: 'rulesCustom.html',
 })
-export class RulesCustomPage {
+export class RulesCustomPage extends RulesStaticPage {
 
   @ViewChild(AutoCompleteComponent) autocomplete: AutoCompleteComponent;
   @ViewChild(Navbar) navBar: Navbar;
   selectedRule: Rule;
   rules: Rule[];
+  customRules: boolean = true;
 
   constructor(public navCtrl: NavController,
     public navParams: NavParams,
-    public rulesAutoCompleteService: RulesCustomAutoCompleteService,
-    private alertCtrl: AlertController,
+    public rulesCustomAutoCompleteService: RulesCustomAutoCompleteService,
+    public rulesStaticAutoCompleteService: RulesStaticAutoCompleteService,
+    public alertCtrl: AlertController,
     public rulesService: RulesService) {
 
-    rulesService.rulesChangedObservable.subscribe(rule => {
+    super(navCtrl, navParams, rulesCustomAutoCompleteService, rulesStaticAutoCompleteService, alertCtrl, rulesService);
+  }
+
+  registerRulesChangedListener() {
+    this.rulesService.rulesChangedObservable.subscribe(rule => {
       if (!rule.released) {
         this.loadRules();
       }
-    })
-  }
-
-  ngOnInit() {
-    this.autocomplete.itemSelected.subscribe(rule => {
-      this.rules = [rule];
-    });
-
-    this.autocomplete.searchbarElem.ionClear.subscribe(() => {
-      this.selectedRule = undefined;
-      this.loadRules();
-    });
-
-    this.loadRules();
-  }
-
-  ionViewDidLoad() {
-    this.navBar.backButtonClick = (e: UIEvent) => {
-      this.navCtrl.parent.viewCtrl.dismiss();
-    };
-  }
-
-  loadRules() {
-    this.rulesService.getRules(true).subscribe(rules => {
-      this.rules = rules;
     })
   }
 
@@ -62,43 +44,5 @@ export class RulesCustomPage {
     })
   }
 
-  createRule() {
-    this.navCtrl.push(RuleEditPage, { rule: {} });
-  }
-
-  editRule(rule: Rule) {
-    this.navCtrl.push(RuleEditPage, { rule: rule });
-  }
-
-  deleteRule(rule: Rule) {
-    this.rulesService.deleteRule(rule.id, true).subscribe(rules => {
-      this.loadRules();
-    })
-  }
-
-  downloadRules() {
-    this.rulesService.downloadRules(true).subscribe(data => {
-      window.open(window.URL.createObjectURL(data), '_system', 'location=yes');
-    })
-  }
-
-  uploadRules(input) {
-    let file: File = input.target.files[0];
-    this.rulesService.uploadRules(true, file).subscribe(
-      data => {
-        this.loadRules();
-      },
-      error => {
-        if (error && error.messages) {
-          error.messages.forEach(message => {
-            if (message.key == "INVALID_RULES") {
-              this.alertCtrl.create({
-                message: message.paramsMap.message,
-                buttons: ['OK']
-              }).present();
-            }
-          })
-        }
-      })
-  }
+  
 }
--- multibanking-app/src/pages/rules-static/rulesStatic.ts
+++ multibanking-app/src/pages/rules-static/rulesStatic.ts
@@ -5,6 +5,7 @@ import { Rule } from '../../api/Rule';
 import { RulesStaticAutoCompleteService } from '../../services/RulesStaticAutoCompleteService';
 import { AutoCompleteComponent } from 'ionic2-auto-complete';
 import { RuleEditPage } from '../rule-edit/ruleEdit';
+import { RulesCustomAutoCompleteService } from '../../services/RulesCustomAutoCompleteService';
 
 
 @Component({
@@ -17,18 +18,16 @@ export class RulesStaticPage {
   @ViewChild(Navbar) navBar: Navbar;
   selectedRule: Rule;
   rules: Rule[];
+  customRules: boolean = false;
 
   constructor(public navCtrl: NavController,
     public navParams: NavParams,
-    private alertCtrl: AlertController,
-    public rulesAutoCompleteService: RulesStaticAutoCompleteService,
+    public rulesCustomAutoCompleteService: RulesCustomAutoCompleteService,
+    public rulesStaticAutoCompleteService: RulesStaticAutoCompleteService,
+    public alertCtrl: AlertController,
     public rulesService: RulesService) {
 
-    rulesService.rulesChangedObservable.subscribe(rule => {
-      if (rule.released) {
-        this.loadRules();
-      }
-    })
+    this.registerRulesChangedListener();
   }
 
   ngOnInit() {
@@ -50,8 +49,16 @@ export class RulesStaticPage {
     };
   }
 
+  registerRulesChangedListener() {
+    this.rulesService.rulesChangedObservable.subscribe(rule => {
+      if (rule.released) {
+        this.loadRules();
+      }
+    })
+  }
+
   loadRules() {
-    this.rulesService.getRules(false).subscribe(rules => {
+    this.rulesService.getRules(this.customRules).subscribe(rules => {
       this.rules = rules;
       this.selectedRule = undefined;
     })
@@ -62,20 +69,44 @@ export class RulesStaticPage {
   }
 
   deleteRule(rule: Rule) {
-    this.rulesService.deleteRule(rule.id, true).subscribe(rules => {
+    this.rulesService.deleteRule(rule.id, this.customRules).subscribe(rules => {
       this.loadRules();
     })
   }
 
   downloadRules() {
-    this.rulesService.downloadRules(false).subscribe(data => {
-      window.open(window.URL.createObjectURL(data));
+    this.rulesService.downloadRules(this.customRules).subscribe(data => {
+      this.showFile(data);
     })
+  }
+
+  showFile(blob) {
+    // It is necessary to create a new blob object with mime-type explicitly set
+    // otherwise only Chrome works like it should
+    var newBlob = new Blob([blob], { type: "application/pdf" })
+
+    // IE doesn't allow using a blob object directly as link href
+    // instead it is necessary to use msSaveOrOpenBlob
+    if (window.navigator && window.navigator.msSaveOrOpenBlob) {
+      window.navigator.msSaveOrOpenBlob(newBlob);
+      return;
+    }
+
+    // For other browsers: 
+    // Create a link pointing to the ObjectURL containing the blob.
+    const data = window.URL.createObjectURL(newBlob);
+    var link = document.createElement('a');
+    link.href = data;
+    link.download = "rules.yaml";
+    link.click();
+    setTimeout(() =>
+      // For Firefox it is necessary to delay revoking the ObjectURL
+      window.URL.revokeObjectURL(data), 100)
   }
 
   uploadRules(input) {
     let file: File = input.target.files[0];
-    this.rulesService.uploadRules(false, file).subscribe(
+    this.rulesService.uploadRules(this.customRules, file).subscribe(
       data => {
         this.loadRules();
       },
