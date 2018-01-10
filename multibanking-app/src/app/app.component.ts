a4981f59ece8479b481abd5895c73557e5881ce2
Renato Gloyer <rgl@adorsys.de>
2018-01-10 14:21:59 +0100

    webapp menu

--- multibanking-app/src/app/app.component.ts
+++ multibanking-app/src/app/app.component.ts
@@ -1,23 +1,35 @@
-import { Component } from "@angular/core";
-import { Platform } from "ionic-angular";
+import { Component, ViewChild } from "@angular/core";
+import { NavController, Platform } from "ionic-angular";
 import { StatusBar } from "@ionic-native/status-bar";
 import { SplashScreen } from "@ionic-native/splash-screen";
 import { BankAccessListPage } from "../pages/bankaccess/bankAccessList.component";
+import { RulesCustomPage } from "../pages/rules-custom/rulesCustom.component";
 
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
 
-  rootPage: any = BankAccessListPage;
+  @ViewChild('content') content: NavController;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  rootPage: any = BankAccessListPage;
+  rulesPage: any = RulesCustomPage;
+
+  constructor(
+    platform: Platform,
+    statusBar: StatusBar,
+    splashScreen: SplashScreen
+  ) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
     });
+  }
+
+  openPage(page) {
+    this.content.setRoot(page);
   }
 }
 
