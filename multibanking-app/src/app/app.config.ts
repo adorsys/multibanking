aec7fc9c1b933664e6155832a1305751f8b0879c
Alexander Geist <age@adorsys.de>
2018-02-16 18:30:28 +0100

    app build

--- multibanking-app/src/app/app.config.ts
+++ multibanking-app/src/app/app.config.ts
@@ -1,14 +1,14 @@
 export const AppConfig = {
   production: false,
   // Openshift
-  // auth_url: 'https://multibanking-keycloak.cloud.adorsys.de/auth',
-  // api_url: 'https://multibanking-service.cloud.adorsys.de/api/v1'
+  auth_url: 'https://multibanking-keycloak.cloud.adorsys.de/auth',
+  api_url: 'https://multibanking-service.cloud.adorsys.de/api/v1'
 
   // multibanking service running from docker container
   // auth_url: 'http://keycloak:8080/auth',
   // api_url: 'http://localhost:8081/api/v1'
 
   // multibanking service running from ide
-  auth_url: 'http://localhost:8080/auth',
-  api_url: 'http://localhost:8081/api/v1'
+  // auth_url: 'http://localhost:8080/auth',
+  // api_url: 'http://localhost:8081/api/v1'
 };
