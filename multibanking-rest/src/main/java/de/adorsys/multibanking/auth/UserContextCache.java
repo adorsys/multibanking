e6e5c6e4e595f7737fbf1c22bfff013947f39f19
fpo <fpo@adorsys.com>
2018-05-06 10:38:01 +0200

    Fixing bug with the user context cache

--- multibanking-rest/src/main/java/de/adorsys/multibanking/auth/UserContextCache.java
+++ multibanking-rest/src/main/java/de/adorsys/multibanking/auth/UserContextCache.java
@@ -3,6 +3,7 @@ package de.adorsys.multibanking.auth;
 import java.lang.reflect.Type;
 import java.util.Collection;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
@@ -10,6 +11,8 @@ import java.util.Set;
 import org.adorsys.docusafe.business.types.complex.DocumentDirectoryFQN;
 import org.adorsys.docusafe.business.types.complex.DocumentFQN;
 import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 
@@ -20,17 +23,25 @@ import com.fasterxml.jackson.core.type.TypeReference;
  *
  */
 public class UserContextCache {
+    private final static Logger LOGGER = LoggerFactory.getLogger(UserContextCache.class);
 	
 	private UserContext userContext;
 	public UserContextCache(UserContext userContext) {
-		super();
 		this.userContext = userContext;
 	}
 
+	/**
+	 * Check for the existence of an entry in the cache. Return empty if object not 
+	 * object not in cache.
+	 * 
+	 * @param documentFQN
+	 * @param valueType
+	 * @return
+	 */
 	public <T> Optional<CacheEntry<T>> cacheHit(DocumentFQN documentFQN, TypeReference<T> valueType){
 		if(!userContext.isCacheEnabled()) return Optional.empty();
-		
 		Map<DocumentFQN, CacheEntry<?>> typeCache = typeCache(valueType);
+		@SuppressWarnings("unchecked")
 		CacheEntry<T> cacheEntry = (CacheEntry<T>) typeCache.get(documentFQN);
 		return Optional.ofNullable(cacheEntry);
 	}
@@ -80,18 +91,20 @@ public class UserContextCache {
 	}
 
 	public void clearCached(DocumentDirectoryFQN dir) {
+        LOGGER.debug("Clearing cache for " + dir);
 		Map<Type, Map<DocumentFQN, CacheEntry<?>>> cache = userContext.getCache();
 		Collection<Map<DocumentFQN,CacheEntry<?>>> values = cache.values();
 		String path = dir.getValue();
 		for (Map<DocumentFQN, CacheEntry<?>> map : values) {
 			Set<DocumentFQN> keySet = map.keySet();
-			Set<DocumentFQN> keyToRemove = map.keySet();
+			Set<DocumentFQN> keyToRemove = new HashSet<>();
 			for (DocumentFQN documentFQN : keySet) {
 				if(StringUtils.startsWith(documentFQN.getValue(), path)){
 					keyToRemove.add(documentFQN);
 				}
 			}
 			for (DocumentFQN documentFQN : keyToRemove) {
+		        LOGGER.debug("Removing from cache Cache " + documentFQN);
 				map.remove(documentFQN);
 			}
 		}
