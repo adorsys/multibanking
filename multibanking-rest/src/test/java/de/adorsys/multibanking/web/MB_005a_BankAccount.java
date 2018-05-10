c7a1e8c930187ffe57b65c542be1c9b9671e5588
fpo <fpo@adorsys.com>
2018-05-10 18:07:16 +0200

    Improving tests on synchyng bookings

--- multibanking-rest/src/test/java/de/adorsys/multibanking/web/MB_005a_BankAccount.java
+++ multibanking-rest/src/test/java/de/adorsys/multibanking/web/MB_005a_BankAccount.java
@@ -2,6 +2,7 @@ package de.adorsys.multibanking.web;
 
 import java.net.URI;
 import java.util.List;
+import java.util.Map;
 
 import org.junit.Assert;
 import org.junit.Test;
@@ -10,6 +11,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.web.util.UriComponentsBuilder;
+
+import de.adorsys.multibanking.domain.BankAccessData;
+import de.adorsys.multibanking.domain.BankAccountData;
+import de.adorsys.multibanking.domain.BookingFile;
+import de.adorsys.multibanking.domain.UserData;
 
 /**
  * Created by peter on 07.05.18 at 08:36.
@@ -23,12 +29,12 @@ import de.adorsys.multibanking.web.base.entity.BankAccountID;
  * https://wiki.adorsys.de/display/DOC/Multibanking-Rest+Tests
  */
 @RunWith(SpringRunner.class)
-public class MB_005_BankAccount extends MB_BaseTest {
-    private final static Logger LOGGER = LoggerFactory.getLogger(MB_005_BankAccount.class);
+public class MB_005a_BankAccount extends MB_BaseTest {
+    private final static Logger LOGGER = LoggerFactory.getLogger(MB_005a_BankAccount.class);
 
     @Test
     // TODO läuft nicht wegen https://jira.adorsys.de/browse/MUL-272
-    public void test_1() {
+    public void test_synch_bank_accounts_check_booking_files() {
         URI location = MB_004_BankAccess.createBankAccess(this, theBeckerTuple);
         UserDataStructure userDataStructure = MB_004_BankAccess.loadBankAccess(this, location);
         List<BankAccessID> bankAccessIDs = userDataStructure.getBankAccessIDs();
@@ -44,6 +50,13 @@ public class MB_005_BankAccount extends MB_BaseTest {
             this.setNextExpectedStatusCode(204);
             LOGGER.info("PUT TO uri:" + uri);
             this.testRestTemplate.put(uri, pin);
+            
+            List<BankAccountData> bankAccountData = getBankAccountData(this);
+            bankAccountData.forEach(bad -> {
+            	Map<String, BookingFile> bookingFiles = bad.getBookingFiles();
+            	Assert.assertNotNull(bookingFiles);
+            	Assert.assertFalse(bookingFiles.isEmpty());
+            });
         });
 
     }
@@ -51,5 +64,11 @@ public class MB_005_BankAccount extends MB_BaseTest {
     public static UriComponentsBuilder bankAccountPath(MB_BaseTest base, BankAccessID accessID) {
         String basePath = BankAccountController.BASE_PATH;
         return base.path(basePath.replace("{accessId}", accessID.getValue()));
+    }
+    
+    public static List<BankAccountData> getBankAccountData(MB_BaseTest base){
+        UserData userData = MB_003a_UserData.loadUserData(base);
+        BankAccessData bankAccessData = userData.getBankAccesses().get(0);
+        return bankAccessData.getBankAccounts();
     }
 }
