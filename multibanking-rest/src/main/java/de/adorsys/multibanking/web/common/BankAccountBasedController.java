119da9bf83034c41a2ede26badf02eaa99c9e1dc
fpo <fpo@adorsys.com>
2018-05-12 12:52:44 +0200

    Improvements from backend

--- multibanking-rest/src/main/java/de/adorsys/multibanking/web/common/BankAccountBasedController.java
+++ multibanking-rest/src/main/java/de/adorsys/multibanking/web/common/BankAccountBasedController.java
@@ -44,19 +44,6 @@ public abstract class BankAccountBasedController extends BankAccessBasedControll
      */
     protected void checkSynch(String accessId, String accountId){
     	BankAccountData accountData = bankAccountService.loadBankAccount(accessId, accountId);
-    	// 1- Never synched
-    	if(accountData.getSynchStatusTime()==null) return;
-    	
-    	// Ready: then go.
-        if (accountData.getBankAccount().getSyncStatus() == BankAccount.SyncStatus.READY) return;
-        
-        // Pending: then 102
-        if (accountData.getBankAccount().getSyncStatus() == BankAccount.SyncStatus.SYNC)
-        	throw new SyncInProgressException(accessId, accountId);
-        
-        // TODO: When do we need the pending status
-        if (accountData.getBankAccount().getSyncStatus() == BankAccount.SyncStatus.PENDING){
-        	LOGGER.warn("The synch status pending is not expected in this application. We will allow synch to go on.");
-        }
+    	SynchChecker.checkSynch(accountData);
     }
 }
