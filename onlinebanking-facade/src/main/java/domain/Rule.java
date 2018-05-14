15995a414a84b6c7e7a1b7693c8dfb579bf9a34a
Alexander Geist <age@adorsys.de>
2018-05-14 15:35:41 +0200

    new rule enum similarityMatchType

--- onlinebanking-facade/src/main/java/domain/Rule.java
+++ onlinebanking-facade/src/main/java/domain/Rule.java
@@ -8,11 +8,16 @@ import lombok.Data;
 @Data
 public class Rule {
 
+    public enum SIMILARITY_MATCH_TYPE {
+        MANDATE_REFERENCE, REFERENCE_NAME, PURPOSE
+    }
+
     private String ruleId;
     private String mainCategory;
     private String subCategory;
     private String specification;
 
+    private SIMILARITY_MATCH_TYPE similarityMatchType;
     private String creditorId;
     private String expression;
     private String receiver;
