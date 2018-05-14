9ad0444accfaa9565bb4d0f19881a81c6c56ce9b
pspex <pspex@adorsys.de>
2018-05-14 10:15:43 +0200

    MUL-271 Keine Dependencies mehr zum Datenmodell aus src/main

--- multibanking-rest/src/test/java/de/adorsys/multibanking/web/MB_006_BookingsTest.java
+++ multibanking-rest/src/test/java/de/adorsys/multibanking/web/MB_006_BookingsTest.java
@@ -1,62 +1,48 @@
 package de.adorsys.multibanking.web;
 
-import de.adorsys.multibanking.domain.BankAccessData;
-import de.adorsys.multibanking.domain.BankAccountData;
-import de.adorsys.multibanking.domain.BookingEntity;
-import de.adorsys.multibanking.domain.BookingFile;
-import de.adorsys.multibanking.web.account.BookingController;
 import de.adorsys.multibanking.web.base.entity.BankAccessID;
 import de.adorsys.multibanking.web.base.entity.BankAccountID;
+import de.adorsys.multibanking.web.base.entity.UserDataStructure;
 import org.junit.Assert;
-import org.junit.Assume;
 import org.junit.Test;
 import org.springframework.http.ResponseEntity;
 
 import java.net.URI;
 import java.util.List;
-import java.util.Map;
+import java.util.Optional;
 
 /**
  * Created by peter on 08.05.18 at 15:49.
  */
 public class MB_006_BookingsTest extends MB_BaseTest {
-     @Test
-    public void load_synched_account_retunrs_list_of_booking_per_period() {
-     	BankAccessData accessData = MB_005_BankAccountTest.createAccessDataWith2BankAccounts(this);
-     	BankAccessID bankAccessID = new BankAccessID(accessData.getBankAccess().getId());
-     	accessData.getBankAccounts().forEach(accountData -> {
-     		BankAccountID bankAccountID = new BankAccountID(accountData.getBankAccount().getId());
-     		MB_005_BankAccountTest.syncBankAccount204(this, bankAccessID, bankAccountID);
-     	});
+	public final static String BOOKING_URI = "/api/v1/bankaccesses/{accessId}/accounts/{accountId}/bookings";
 
-        List<BankAccountData> bankAccountData = MB_005_BankAccountTest.loadAccessDataWith2BankAccounts(this).getBankAccounts();
-        final CatgoryCount cc = new CatgoryCount();
-        bankAccountData.forEach(bad -> {
-        	BankAccountID bankAccountID = new BankAccountID(bad.getBankAccount().getId());
-        	LOGGER.info("found bank-AccountID:" + bankAccountID);
-        	Map<String, BookingFile> bookingFiles = bad.getBookingFiles();
-        	Assume.assumeNotNull(bookingFiles);
-        	Assume.assumeFalse(bookingFiles.isEmpty());
-            bookingFiles.values().forEach(bookingFile -> {
-            	String period = bookingFile.getPeriod();
-            	URI uri = path(BookingController.BASE_PATH).queryParam("period",period).build(bankAccessID.getValue(), bankAccountID.getValue());
-            	this.setNextExpectedStatusCode(200);
-            	LOGGER.info("GET TO uri:" + uri);
-            	ResponseEntity<BookingEntity[]> bookingResponse = this.testRestTemplate.getForEntity(uri, BookingEntity[].class);
-            	BookingEntity[] bookings = bookingResponse.getBody();
-            	for (int i = 0; i < bookings.length; i++) {
-					BookingEntity booking = bookings[i];
-					if(booking.getBookingCategory()!=null){
-						cc.categoryFound = cc.categoryFound+1;
-						LOGGER.info("booking with Category :" + booking.getBookingCategory().getMainCategory());
-					}
-				}
-            });
-        });
-        Assert.assertTrue(cc.categoryFound>0);
-    }
-     
-     static class CatgoryCount{
-    	 int categoryFound;
-     }
+	@Test
+	public void load_synched_account_retunrs_list_of_booking_per_period() {
+		URI location = MB_004_BankAccessTest.createBankAccess(this, theBeckerTuple);
+		UserDataStructure userDataStructure = MB_005_BankAccountTest.accessDataWith2BankAccounts(this, location);
+		BankAccessID firstBankAccessID = userDataStructure.getBankAccessIDs().get(0);
+		for (BankAccountID bankAccountID : userDataStructure.getBankAccountIDs(firstBankAccessID)) {
+			Assert.assertFalse(userDataStructure.getBookingPeriods(firstBankAccessID, bankAccountID).isPresent());
+		}
+
+		for (BankAccountID bankAccountID : userDataStructure.getBankAccountIDs(firstBankAccessID)) {
+			MB_005_BankAccountTest.syncBankAccount204(this, firstBankAccessID, bankAccountID);
+		}
+
+		userDataStructure = MB_005_BankAccountTest.accessDataWith2BankAccounts(this, location);
+		for (BankAccountID bankAccountID : userDataStructure.getBankAccountIDs(firstBankAccessID)) {
+			Optional<List<String>> bookingPeriods = userDataStructure.getBookingPeriods(firstBankAccessID, bankAccountID);
+			Assert.assertTrue(bookingPeriods.isPresent());
+			for (String period : bookingPeriods.get()) {
+				LOGGER.info("period is " + period);
+				URI uri = path(BOOKING_URI).queryParam("period", period).build(firstBankAccessID.getValue(), bankAccountID.getValue());
+				this.setNextExpectedStatusCode(200);
+				LOGGER.info("GET TO uri:" + uri);
+				ResponseEntity<String> bookings = this.testRestTemplate.getForEntity(uri, String.class);
+				LOGGER.info("bookings:" + bookings.getBody());
+			}
+		}
+	}
+
 }
--- multibanking-rest/src/test/java/de/adorsys/multibanking/web/MB_007_AnalyticsTest.java
+++ multibanking-rest/src/test/java/de/adorsys/multibanking/web/MB_007_AnalyticsTest.java
@@ -1,48 +1,48 @@
-package de.adorsys.multibanking.web;
-
-import de.adorsys.multibanking.domain.BankAccessData;
-import de.adorsys.multibanking.domain.BankAccountData;
-import de.adorsys.multibanking.domain.BookingFile;
-import de.adorsys.multibanking.web.account.BookingController;
-import de.adorsys.multibanking.web.base.entity.BankAccessID;
-import de.adorsys.multibanking.web.base.entity.BankAccountID;
-import org.junit.Assume;
-import org.junit.Test;
-import org.springframework.http.ResponseEntity;
-
-import java.net.URI;
-import java.util.List;
-import java.util.Map;
-
-/**
- * @author fpo
- */
-public class MB_007_AnalyticsTest extends MB_BaseTest {
-     @Test
-    public void load_synched_account_retunrs_list_of_booking_with_analytics() {
-     	BankAccessData accessData = MB_005_BankAccountTest.createAccessDataWith2BankAccounts(this);
-     	BankAccessID bankAccessID = new BankAccessID(accessData.getBankAccess().getId());
-     	accessData.getBankAccounts().forEach(accountData -> {
-     		BankAccountID bankAccountID = new BankAccountID(accountData.getBankAccount().getId());
-     		MB_005_BankAccountTest.syncBankAccount204(this, bankAccessID, bankAccountID);
-     	});
-
-        List<BankAccountData> bankAccountData = MB_005_BankAccountTest.loadAccessDataWith2BankAccounts(this).getBankAccounts();
-        bankAccountData.forEach(bad -> {
-        	BankAccountID bankAccountID = new BankAccountID(bad.getBankAccount().getId());
-        	LOGGER.info("found bank-AccountID:" + bankAccountID);
-        	Map<String, BookingFile> bookingFiles = bad.getBookingFiles();
-        	Assume.assumeNotNull(bookingFiles);
-        	Assume.assumeFalse(bookingFiles.isEmpty());
-        	
-            bookingFiles.values().forEach(bookingFile -> {
-            	String period = bookingFile.getPeriod();
-            	URI uri = path(BookingController.BASE_PATH).queryParam("period",period).build(bankAccessID.getValue(), bankAccountID.getValue());
-            	this.setNextExpectedStatusCode(200);
-            	LOGGER.info("GET TO uri:" + uri);
-            	ResponseEntity<String> bookings = this.testRestTemplate.getForEntity(uri, String.class);
-            	LOGGER.info("bookings:" + bookings.getBody());
-            });
-        });
-    }
-}
+//package de.adorsys.multibanking.web;
+//
+//import de.adorsys.multibanking.domain.BankAccessData;
+//import de.adorsys.multibanking.domain.BankAccountData;
+//import de.adorsys.multibanking.domain.BookingFile;
+//import de.adorsys.multibanking.web.account.BookingController;
+//import de.adorsys.multibanking.web.base.entity.BankAccessID;
+//import de.adorsys.multibanking.web.base.entity.BankAccountID;
+//import org.junit.Assume;
+//import org.junit.Test;
+//import org.springframework.http.ResponseEntity;
+//
+//import java.net.URI;
+//import java.util.List;
+//import java.util.Map;
+//
+///**
+// * @author fpo
+// */
+//public class MB_007_AnalyticsTest extends MB_BaseTest {
+//     @Test
+//    public void load_synched_account_retunrs_list_of_booking_with_analytics() {
+//     	BankAccessData accessData = MB_005_BankAccountTest.createAccessDataWith2BankAccounts(this);
+//     	BankAccessID bankAccessID = new BankAccessID(accessData.getBankAccess().getId());
+//     	accessData.getBankAccounts().forEach(accountData -> {
+//     		BankAccountID bankAccountID = new BankAccountID(accountData.getBankAccount().getId());
+//     		MB_005_BankAccountTest.syncBankAccount204(this, bankAccessID, bankAccountID);
+//     	});
+//
+//        List<BankAccountData> bankAccountData = MB_005_BankAccountTest.loadAccessDataWith2BankAccounts(this).getBankAccounts();
+//        bankAccountData.forEach(bad -> {
+//        	BankAccountID bankAccountID = new BankAccountID(bad.getBankAccount().getId());
+//        	LOGGER.info("found bank-AccountID:" + bankAccountID);
+//        	Map<String, BookingFile> bookingFiles = bad.getBookingFiles();
+//        	Assume.assumeNotNull(bookingFiles);
+//        	Assume.assumeFalse(bookingFiles.isEmpty());
+//
+//            bookingFiles.values().forEach(bookingFile -> {
+//            	String period = bookingFile.getPeriod();
+//            	URI uri = path(BookingController.BASE_PATH).queryParam("period",period).build(bankAccessID.getValue(), bankAccountID.getValue());
+//            	this.setNextExpectedStatusCode(200);
+//            	LOGGER.info("GET TO uri:" + uri);
+//            	ResponseEntity<String> bookings = this.testRestTemplate.getForEntity(uri, String.class);
+//            	LOGGER.info("bookings:" + bookings.getBody());
+//            });
+//        });
+//    }
+//}
