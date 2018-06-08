726f06dc2c334f8d2148c19606fbac21c3489e17
Alexander Geist <age@adorsys.de>
2018-06-08 12:01:40 +0200

    datev env config

---
+++ multibanking-app/src/env/env.datev-dev.ts
@@ -0,0 +1,7 @@
+import { Environment } from "./env.interface";
+
+export const ENV: Environment = {
+    auth_url: 'https://keycloak-datevsb.cloud.adorsys.de/auth',
+    api_url: 'https://datevsb-multibanking.cloud.adorsys.de/api/v1',
+    smartanalytics_url: 'https://smartanalytics-datevsb.cloud.adorsys.de/api/v1'
+}
