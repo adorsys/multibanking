eca866227d19bca51db12b8faa078892cfeaaaf9
Alexander Geist <age@adorsys.de>
2018-06-18 10:01:41 +0200

    changed multibanking-app smartanalytics_url

--- multibanking-app/src/env/env.ts
+++ multibanking-app/src/env/env.ts
@@ -3,5 +3,5 @@ import { Environment } from "./env.interface";
 export const ENV: Environment = {
   auth_url: 'http://localhost:8080/auth',
   api_url: 'http://localhost:8081/api/v1',
-  smartanalytics_url: 'http://localhost:8082/api/v1'
+  smartanalytics_url: 'http://localhost:8081/api/v1'
 }
