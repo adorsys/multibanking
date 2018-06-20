3628db00eb8fce6460b3b0c13d7f9e25b83ecf93
igor <ihorpetroff@gmail.com>
2018-06-20 15:31:42 +0200

    MUL-216: bookings job checking

--- onlinebanking-hbci4java/src/main/java/exception/AccessLockedAccountException.java
+++
@@ -1,7 +0,0 @@
-package exception;
-
-public class AccessLockedAccountException extends RuntimeException {
-    public AccessLockedAccountException() {
-        super("Zugang gesperrt, Freischaltung mit TAN erforderlich.");
-    }
-}
