37718e2500e220df78bbe21ca42142d7c44c3562
cga <cga@adorsys.de>
2018-06-28 15:24:59 +0200

    remove logo

--- multibanking-app/src/env/env.ts
+++ multibanking-app/src/env/env.ts
@@ -1,7 +1,9 @@
 import { Environment } from "./env.interface";
 
 export const ENV: Environment = {
-  auth_url: 'http://localhost:8080/auth',
-  api_url: 'http://localhost:8081/api/v1',
-  smartanalytics_url: 'http://localhost:8081/api/v1'
+  auth_url: 'https://flip-auth-dev.cloud.adorsys.de/auth',
+  api_url: 'https://flip-multibanking-dev.cloud.adorsys.de/api/v1',
+  smartanalytics_url: 'https://flip-multibanking-dev.cloud.adorsys.de/api/v1'
 }
+
+
