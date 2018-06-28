74793378eb492c692f727a52756a2d6613f61b97
cga <cga@adorsys.de>
2018-06-28 15:30:51 +0200

    cleanup

--- multibanking-app/src/env/env.ts
+++ multibanking-app/src/env/env.ts
@@ -1,9 +1,7 @@
 import { Environment } from "./env.interface";
 
 export const ENV: Environment = {
-  auth_url: 'https://flip-auth-dev.cloud.adorsys.de/auth',
-  api_url: 'https://flip-multibanking-dev.cloud.adorsys.de/api/v1',
-  smartanalytics_url: 'https://flip-multibanking-dev.cloud.adorsys.de/api/v1'
+  auth_url: 'http://localhost:8080/auth',
+  api_url: 'http://localhost:8081/api/v1',
+  smartanalytics_url: 'http://localhost:8081/api/v1'
 }
-
-
