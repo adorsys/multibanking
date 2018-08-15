2c77e236ae8187989dfb6b3345f53ba71a67cc3d
Alexander Geist <age@adorsys.de>
2018-08-15 10:41:21 +0200

    app new env flip-dev

---
+++ multibanking-app/src/env/env.flip-dev.ts
@@ -0,0 +1,8 @@
+import { Environment } from "./env.interface";
+
+export const ENV: Environment = {
+  auth_url: 'https://flip-auth-dev.cloud.adorsys.de/auth',
+  api_url: 'https://flip-multibanking-dev.cloud.adorsys.de/api/v1',
+  smartanalytics_url: 'https://flip-multibanking-dev.cloud.adorsys.de/api/v1'
+
+}
