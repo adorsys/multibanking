da407db3d8095ba19ce9db4504d4b0e90e05f4b8
Alexander Geist <age@adorsys.de>
2018-11-27 09:51:11 +0100

    spring 2.1.0

--- multibanking-app/src/env/env.ts
+++ multibanking-app/src/env/env.ts
@@ -2,6 +2,6 @@ import { Environment } from "./env.interface";
 
 export const ENV: Environment = {
   auth_url: 'http://localhost:8080/auth',
-  api_url: 'http://localhost:8081/api/v1',
-  smartanalytics_url: 'http://localhost:8081/api/v1'
+  api_url: 'http://localhost:8088/api/v1',
+  smartanalytics_url: 'http://localhost:8088/api/v1'
 }
