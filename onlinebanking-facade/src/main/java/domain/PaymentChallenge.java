20fbbf1b1c0d42af52b1db2ff46d7196530f9f9b
Alexander Geist <age@adorsys.de>
2018-11-28 15:32:55 +0100

    fixed missing NoArgsConstructor for PaymentChallenge class

--- onlinebanking-facade/src/main/java/domain/PaymentChallenge.java
+++ onlinebanking-facade/src/main/java/domain/PaymentChallenge.java
@@ -1,13 +1,17 @@
 package domain;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /**
  * Created by alexg on 10.11.17.
  */
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class PaymentChallenge {
 
     private String title;
