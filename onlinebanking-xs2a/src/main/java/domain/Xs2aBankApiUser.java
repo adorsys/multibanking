bdf5d6231349cbe92a37838d1dd1fabf4e65a11b
Serhii Petrychenko <spe@adorsys.com.ua>
2019-03-13 14:46:48 +0200

    implement get balances from xs2a

---
+++ onlinebanking-xs2a/src/main/java/domain/Xs2aBankApiUser.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2018-2018 adorsys GmbH & Co KG
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package domain;
+
+import java.util.HashMap;
+
+public class Xs2aBankApiUser extends BankApiUser {
+
+    public Xs2aBankApiUser(String consentId) {
+        setConsentId(consentId);
+    }
+
+    public Xs2aBankApiUser() {
+    }
+
+    private static final String CONSENT_ID = "CONSENT_ID";
+
+    public String getConsentId() {
+        return getProperties().get(CONSENT_ID);
+    }
+
+    public void setConsentId(String consentId) {
+        if (getProperties() == null) {
+            setProperties(new HashMap<>());
+        }
+        getProperties().put(CONSENT_ID, consentId);
+    }
+}
