7e798b5ac534663756fccca519531ef384aac4f2
Linus See <lse@adorsys.de>
2019-04-03 19:34:41 +0200

    feat: db and model for anonymized bookings

---
+++ multibanking-pers-spi/src/main/java/de/adorsys/multibanking/domain/MlAnonymizedBookingEntity.java
@@ -0,0 +1,26 @@
+package de.adorsys.multibanking.domain;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.Data;
+
+@Data
+@Document
+public class MlAnonymizedBookingEntity {
+	
+	@Id
+    private String id;
+	@Indexed
+	private final String userId;
+    private final String transactionPurpose;
+    private final String amountBin;
+    private final String amountRemainder;
+    private final String creditorId;
+    private final String referenceName;
+    private final String executionDate;
+    private final String mainCategory;
+    private final String subCategory;
+    private final String specification;
+}
