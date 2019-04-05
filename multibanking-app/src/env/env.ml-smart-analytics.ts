eb2be581b0ed01b4e929a1d801bd3e397f741cfe
Linus See <lse@adorsys.de>
2019-04-05 15:15:27 +0200

    feat: env for ml

---
+++ multibanking-app/src/env/env.ml-smart-analytics.ts
@@ -0,0 +1,7 @@
+import { Environment } from "./env.interface";
+
+export const ENV: Environment = {
+  auth_url: 'https://adorsys-multibanking-keycloak-tax-aid-dev.cloud.adorsys.de',
+  api_url: 'https://adorsys-multibanking-tax-aid-dev.cloud.adorsys.de',
+  smartanalytics_url: 'https://adorsys-smartanalytics-tax-aid-dev.cloud.adorsys.de'
+}
