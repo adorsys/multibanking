6916ebc00ccc40202cba21e340a619efa6d67d95
Alexander Geist <age@adorsys.de>
2019-05-04 22:20:53 +0200

    app highlight disabled contracts

--- multibanking-app/src/services/rulesStaticAutoComplete.service.ts
+++ multibanking-app/src/services/rulesStaticAutoComplete.service.ts
@@ -1,5 +1,4 @@
 import { Injectable } from '@angular/core';
-import { Http, Response } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 import { AutoCompleteService } from "ionic2-auto-complete";
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
