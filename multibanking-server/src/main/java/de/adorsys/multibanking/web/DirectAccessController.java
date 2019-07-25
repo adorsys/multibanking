2475893ccc1aceb6aad65d0adec363b2c3266876
Martin Fochler <mafo.work@googlemail.com>
2019-07-25 12:03:25 +0200

    add a bit of logging to see more steps in logs to identify certain problems

--- multibanking-server/src/main/java/de/adorsys/multibanking/web/DirectAccessController.java
+++ multibanking-server/src/main/java/de/adorsys/multibanking/web/DirectAccessController.java
@@ -15,6 +15,8 @@ import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -29,6 +31,8 @@ import java.util.UUID;
 @RequiredArgsConstructor
 @RequestMapping(path = "api/v1/direct")
 public class DirectAccessController {
+
+    private Logger logger = LoggerFactory.getLogger(getClass());
 
     private final BookingMapper bookingMapper;
     private final BankApiMapper bankApiMapper;
@@ -53,6 +57,8 @@ public class DirectAccessController {
     public ResponseEntity<LoadBankAccountsResponse> loadBankAccounts(@RequestBody BankAccessTO bankAccess,
                                                                      @RequestParam(required = false) BankApiTO bankApi) {
         //temporary user, will be deleted after x minutes
+        logger.debug("process start > load bank account list");
+        logger.debug("save temporary user to db");
         UserEntity userEntity = new UserEntity();
         userEntity.setId(UUID.randomUUID().toString());
         userEntity.setExpireUser(LocalDateTime.now().plusMinutes(thresholdTemporaryData));
@@ -66,9 +72,12 @@ public class DirectAccessController {
         LoadBankAccountsResponse response = new LoadBankAccountsResponse();
 
         try {
+            logger.debug("load bank account list from bank");
             List<BankAccountEntity> bankAccounts = bankAccountService.loadBankAccountsOnline(bankAccessEntity,
                 bankApiMapper.toBankApi(bankApi));
 
+
+            logger.debug("save bank account list to db");
             bankAccessEntity.setPin(null);
             bankAccessEntity.setTemporary(true);
             bankAccessEntity.setUserId(userEntity.getId());
@@ -79,6 +88,7 @@ public class DirectAccessController {
                 bankAccountRepository.save(account);
             });
 
+            logger.debug("process finished < return bank account list");
             response.setBankAccounts(bankAccountMapper.toBankAccountTOs(bankAccounts));
             return new ResponseEntity<>(response, HttpStatus.OK);
 
@@ -95,10 +105,13 @@ public class DirectAccessController {
     @PutMapping("/bookings")
     public ResponseEntity<LoadBookingsResponse> loadBookings(@RequestBody LoadBookingsRequest loadBookingsRequest,
                                                              @RequestParam(required = false) BankApiTO bankApi) {
+        logger.debug("process start > load booking list");
+        logger.debug("load bank access");
         BankAccessEntity bankAccessEntity = bankAccessRepository.findOne(loadBookingsRequest.getAccessId())
             .orElseThrow(() -> new ResourceNotFoundException(BankAccessEntity.class,
                 loadBookingsRequest.getAccessId()));
 
+        logger.debug("load bank account");
         BankAccountEntity bankAccountEntity = bankAccountRepository.findOne(loadBookingsRequest.getAccountId())
             .orElseThrow(() -> new ResourceNotFoundException(BankAccountEntity.class,
                 loadBookingsRequest.getAccountId()));
@@ -106,11 +119,13 @@ public class DirectAccessController {
         LoadBookingsResponse loadBookingsResponse = new LoadBookingsResponse();
 
         try {
+            logger.debug("load booking list from bank");
             List<BookingEntity> bookings = bookingService.syncBookings(bankAccessEntity, bankAccountEntity,
                 bankApiMapper.toBankApi(bankApi), loadBookingsRequest.getPin());
             loadBookingsResponse.setBookings(bookingMapper.toBookingTOs(bookings));
             loadBookingsResponse.setBalances(balancesMapper.toBalancesReportTO(bankAccountEntity.getBalances()));
 
+            logger.debug("process finished < return booking list");
             return new ResponseEntity<>(loadBookingsResponse, HttpStatus.OK);
         } catch (ExternalAuthorisationRequiredException e) {
             loadBookingsResponse.setConsent(consentMapper.toConsentTO(e.getConsent()));
