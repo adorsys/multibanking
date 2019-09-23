1a241cdc9abf208497cf0d4a9212da36a8a6bd47
Alexander Geist <age@adorsys.de>
2019-09-23 14:15:40 +0200

    fixed dev environment base url

--- multibanking-app/src/environments/environment.dev.ts
+++ multibanking-app/src/environments/environment.dev.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   isApp: false,
-  base_url: 'https://https://dev-bankinggateway-multibanking-multibankingapp.cloud.adorsys.de',
+  base_url: 'https://dev-bankinggateway-multibanking-multibankingapp.cloud.adorsys.de',
   auth_url: 'https://dev-bankinggateway-multibanking-multibankingkeycloak.cloud.adorsys.de/auth',
   api_url: 'https://dev-bankinggateway-multibanking-multibankingservice.cloud.adorsys.de/api/v1',
   smartanalytics_url: 'https://dev-bankinggateway-multibanking-multibankingservice.cloud.adorsys.de/api/v1',
