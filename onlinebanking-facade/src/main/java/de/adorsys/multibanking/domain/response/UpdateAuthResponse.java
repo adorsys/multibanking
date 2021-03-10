53ca9722a4e6af8c09ec610f380234b63258f434
Christian Brandenstein <cbr@adorsys.de>
2021-03-10 16:13:49 +0100

    pass through psuMessage

--- onlinebanking-facade/src/main/java/de/adorsys/multibanking/domain/response/UpdateAuthResponse.java
+++ onlinebanking-facade/src/main/java/de/adorsys/multibanking/domain/response/UpdateAuthResponse.java
@@ -31,5 +31,6 @@ public class UpdateAuthResponse extends AbstractResponse {
     private final ScaStatus scaStatus;
     private List<TanTransportType> scaMethods;
     private ChallengeData challenge;
+    private String psuMessage;
 
 }
