6c6d813a9be557c07abf2bdc7805ecba6a2fc379
Christian Brandenstein <cbr@adorsys.de>
2021-06-25 14:46:25 +0200

    remove travis

--- .travis/deploy_openshift_multibanking_service.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-set -e
-docker login ${OPENSHIFT_REGISTRY} -u ${OPENSHIFT_USER} -p ${OPENSHIFT_TOKEN}
-docker build -t ${OPENSHIFT_REGISTRY}/${OPENSHIFT_NAMESPACE}/multibanking-service:${TAG} multibanking-server/
-docker push ${OPENSHIFT_REGISTRY}/${OPENSHIFT_NAMESPACE}/multibanking-service:${TAG}
